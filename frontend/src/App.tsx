41efeb269f1623cdc03554d8c3c3bc8ac6f8ec36
Tonny Huynh <tonnyhuynh02@gmail.com>
2025-06-27 00:57:55 +0200

    Fixed architecture issues, such as application breaking due to compatibility issues, frontend added transaction component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TransactionForm from './components/TransactionForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Add a transaction</h2>
+      <TransactionForm type="expense" amount="" category="" note="" />
     </div>
   );
 }
---
+++ frontend/src/components/TransactionForm.tsx
@@ -0,0 +1,84 @@
+import {useState} from "react";
+import axios from "axios";
+
+//Our attributes
+interface Props {
+    type: 'expense' | 'income',
+    amount: string,
+    category: string,
+    note: string
+}
+
+//Main component
+function TransactionForm(types : Props) {
+    const [formData, setFormData] = useState(types);
+
+    //Subcomponent, When user types something , event accepterar både input och select element
+    function handleChange(event : React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+        setFormData({
+            ...formData,
+            [event.target.name] : event.target.value
+        });
+    }
+
+    //When submitting or saving
+    async function handleSubmit(e : React.FormEvent<HTMLFormElement>) {
+        e.preventDefault(); //Stop page from refreshing
+
+    try {
+        //Sending data to the backend
+        await axios.post("http://localhost:5000/api/transactions", {
+            ...formData,
+            amount: parseFloat(formData.amount) //Converting String to number
+        });
+
+        alert("Transaction saved!");
+
+        //Reset
+        setFormData({
+            type: 'expense',
+            amount: '',
+            category: '',
+            note: ''
+
+        });
+
+    } catch (error) {
+        alert(" could not save transaction.");
+        console.error(error);
+    }
+    }
+
+    return <>
+        <form onSubmit = {handleSubmit}>
+        <label>
+            Type:
+            <select name="type" value = {formData.type} onChange= {handleChange}>
+                <option value="income">Income</option>
+                <option value="expense">Expense</option>
+            </select>
+        </label>
+
+        <label>
+            amount:
+            <input name = "amount" value = {formData.amount} onChange = {handleChange} required />
+        </label>
+
+        <label>
+            category:
+            <input name="category" value={formData.category} onChange = {handleChange} />
+        </label>
+
+        <label>
+            note:
+            <input name="note" value = {formData.note} onChange = {handleChange} />
+        </label>
+
+        <button type ="submit"> Save</button>
+        </form>
+
+    </>
+    
+}
+
+export default TransactionForm;
